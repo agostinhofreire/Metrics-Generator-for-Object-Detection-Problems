993dce7b88a5a4162fe4f98b7bdd2e8470916c53
Agostinho Júnior <agostinho.junior@avantia.com.br>
2021-04-30 12:55:55 -0300

    Project creation

---
+++ README.md
@@ -0,0 +1,68 @@
+Metrics Generator for Object Detection Problems
+====
+
+Use this project to generate the metrics results for your object detection problem.
+
+## **Running the Metriscs Generator**
+
+### **1st Step:**
+
+Make sure you have installed all project dependencies, run in the root folder:
+
+    pip install -r deploy/requirements.txt
+
+The entire project uses `Python 3.6.13`.
+
+### **2nd Step:**
+
+You must provide two .txt files containing the dataset ground truth and the model's predictions informations. The files must be formatted with the following pattern.
+
+`The ground truth file`
+
+```
+img1.jpg class_id xmin ymin xmax ymax class_id xmin ymin xmax ymax class_id xmin ymin xmax ymax ...
+img2.jpg class_id xmin ymin xmax ymax class_id xmin ymin xmax ymax class_id xmin ymin xmax ymax ...
+img3.jpg class_id xmin ymin xmax ymax class_id xmin ymin xmax ymax class_id xmin ymin xmax ymax ...
+img4.jpg class_id xmin ymin xmax ymax class_id xmin ymin xmax ymax class_id xmin ymin xmax ymax ...
+```
+`The predictions file`
+
+```
+img1.jpg class_id confidence xmin ymin xmax ymax class_id confidence xmin ymin xmax ymax class_id confidence xmin ymin xmax ymax ...
+img2.jpg class_id confidence xmin ymin xmax ymax class_id confidence xmin ymin xmax ymax class_id confidence xmin ymin xmax ymax ...
+img3.jpg class_id confidence xmin ymin xmax ymax class_id confidence xmin ymin xmax ymax class_id confidence xmin ymin xmax ymax ...
+img4.jpg class_id confidence xmin ymin xmax ymax class_id confidence xmin ymin xmax ymax class_id confidence xmin ymin xmax ymax ...
+
+```
+**Note:**
+
++ `class_id`: interger that represents a class in your dataset
++ `(xmin, ymin)`: bounding box top-left coordinate
++ `(xmax, ymax)`: bounding box bottom-right coordinate
++ `confidence`: prediction's confidence probability
+
+### **3rd Step:**
+
+Edit the Config.cfg file stored in the core folder.
+```python
+[MODEL]
+classes = ['capacitor', 'diode', 'ic', 'inductor', 'resistor', 'transistor'] # your dataset classes
+min_overlap = 0.5 # minimum overlap between bounding boxes
+classes_to_ignore = [] # list of classes that you want to ignore
+
+[RESULTS]
+save_plots = True # save generated graphs
+show_plots = False # plot generated graphs
+dataset_name = "pcb-dataset" # dataset name
+
+[DATA]
+path_gt_file = "data/ground_truth.txt" # path to the ground truth file
+path_pred_file = "data/predicted.txt" # path to the predictions file
+```
+### **4th Step:**
+
+Execute de code by running:
+
+    python main.py
+
+Finally, all results will be saved in the root folder.
---
+++ main.py
@@ -0,0 +1,437 @@
+import glob
+import json
+import os
+import shutil
+
+import matplotlib.pyplot as plt
+
+from utils.Utils import *
+
+from configparser import ConfigParser
+import ast
+
+config_object = ConfigParser()
+
+try:
+  config_object.read("core/Config.cfg")
+except:
+  error("Config file not found in the core folder!")
+
+try:
+  model_config = config_object["MODEL"]
+
+  classes = ast.literal_eval(model_config["classes"])
+  min_overlap = ast.literal_eval(model_config["min_overlap"])
+  classes_to_ignore = ast.literal_eval(model_config["classes_to_ignore"])
+
+  results_config = config_object["RESULTS"]
+
+  save_plots = ast.literal_eval(results_config["save_plots"])
+  show_plots = ast.literal_eval(results_config["show_plots"])
+  dataset_name = ast.literal_eval(results_config["dataset_name"])
+
+  data_config = config_object["DATA"]
+
+  path_gt_file = ast.literal_eval(data_config["path_gt_file"])
+  path_pred_file = ast.literal_eval(data_config["path_pred_file"])
+except:
+  error("Verify the Config.cfg file!")
+
+if not os.path.exists(path_gt_file):
+  error("Ground truth file not found!")
+if not os.path.exists(path_pred_file):
+  error("Predicted file not found!")
+
+
+create_p_gt_folders(path_gt_file, path_pred_file, dataset_name, classes)
+
+"""
+ Create a "tmp_files/" and "results/" directory
+"""
+tmp_files_path = "tmp_files"
+if not os.path.exists(tmp_files_path): # if it doesn't exist already
+  os.makedirs(tmp_files_path)
+results_files_path = "results-" + str(dataset_name)
+if os.path.exists(results_files_path): # if it exist already
+  # reset the results directory
+  shutil.rmtree(results_files_path)
+
+os.makedirs(results_files_path)
+if save_plots:
+  os.makedirs(results_files_path + "/classes")
+
+
+"""
+ Ground-Truth
+   Load each of the ground-truth files into a temporary ".json" file.
+   Create a list of all the class names present in the ground-truth (gt_classes).
+"""
+# get a list with the ground-truth files
+ground_truth_files_list = glob.glob(f'data/ground-truth-{dataset_name}/*.txt')
+if len(ground_truth_files_list) == 0:
+  error("Error: No ground-truth files found!")
+ground_truth_files_list.sort()
+# dictionary with counter per class
+gt_counter_per_class = {}
+
+for txt_file in ground_truth_files_list:
+  #print(txt_file)f"predicted-{name}"
+  file_id = txt_file.split(".txt",1)[0]
+  file_id = os.path.basename(os.path.normpath(file_id))
+  # check if there is a correspondent predicted objects file
+  if not os.path.exists(f"data/predicted-{dataset_name}/" + file_id + ".txt"):
+    error_msg = f"Error. File not found: data/predicted-{dataset_name}/" +  file_id + ".txt\n"
+    error_msg += "(You can avoid this error message by running extra/intersect-gt-and-pred.py)"
+    error(error_msg)
+  lines_list = file_lines_to_list(txt_file)
+  # create ground-truth dictionary
+  bounding_boxes = []
+  is_difficult = False
+  for line in lines_list:
+    try:
+      if "difficult" in line:
+          class_name, left, top, right, bottom, _difficult = line.split()
+          # class_name = classes[int(class_name_id)]
+          is_difficult = True
+      else:
+          class_name, left, top, right, bottom = line.split()
+          # class_name = classes[int(class_name_id)]
+
+    except ValueError:
+      error_msg = "Error: File " + txt_file + " in the wrong format.\n"
+      error_msg += " Expected: <class_name> <left> <top> <right> <bottom> ['difficult']\n"
+      error_msg += " Received: " + line
+      error_msg += "\n\nIf you have a <class_name> with spaces between words you should remove them\n"
+      error_msg += "by running the script \"remove_space.py\" or \"rename_class.py\" in the \"extra/\" folder."
+      error(error_msg)
+    # check if class is in the classes_to_ignore list, if yes skip
+    if class_name in classes_to_ignore:
+      continue
+    bbox = left + " " + top + " " + right + " " +bottom
+    if is_difficult:
+        bounding_boxes.append({"class_name":class_name, "bbox":bbox, "used":False, "difficult":True})
+        is_difficult = False
+    else:
+        bounding_boxes.append({"class_name":class_name, "bbox":bbox, "used":False})
+        # count that object
+        if class_name in gt_counter_per_class:
+          gt_counter_per_class[class_name] += 1
+        else:
+          # if class didn't exist yet
+          gt_counter_per_class[class_name] = 1
+  # dump bounding_boxes into a ".json" file
+  with open(tmp_files_path + "/" + file_id + "_ground_truth.json", 'w') as outfile:
+    json.dump(bounding_boxes, outfile)
+
+gt_classes = list(gt_counter_per_class.keys())
+# let's sort the classes alphabetically
+gt_classes = sorted(gt_classes)
+n_classes = len(gt_classes)
+#print(gt_classes)
+#print(gt_counter_per_class)
+
+
+
+"""
+ Predicted
+   Load each of the predicted files into a temporary ".json" file.
+"""
+# get a list with the predicted files
+predicted_files_list = glob.glob(f'data/predicted-{dataset_name}/*.txt')
+predicted_files_list.sort()
+
+for class_index, class_name in enumerate(gt_classes):
+  bounding_boxes = []
+  for txt_file in predicted_files_list:
+    #print(txt_file)
+    # the first time it checks if all the corresponding ground-truth files exist
+    file_id = txt_file.split(".txt",1)[0]
+    file_id = os.path.basename(os.path.normpath(file_id))
+    if class_index == 0:
+      if not os.path.exists(f"data/ground-truth-{dataset_name}/" + file_id + ".txt"):
+        error_msg = f"Error. File not found: data/ground-truth-{dataset_name}/" +  file_id + ".txt\n"
+        error_msg += "(You can avoid this error message by running extra/intersect-gt-and-pred.py)"
+        error(error_msg)
+    lines = file_lines_to_list(txt_file)
+    for line in lines:
+      try:
+        tmp_class_name, confidence, left, top, right, bottom = line.split()
+      except ValueError:
+        error_msg = "Error: File " + txt_file + " in the wrong format.\n"
+        error_msg += " Expected: <class_name> <confidence> <left> <top> <right> <bottom>\n"
+        error_msg += " Received: " + line
+        error(error_msg)
+      if tmp_class_name == class_name:
+        #print("match")
+        bbox = left + " " + top + " " + right + " " +bottom
+        bounding_boxes.append({"confidence":confidence, "file_id":file_id, "bbox":bbox})
+        #print(bounding_boxes)
+  # sort predictions by decreasing confidence
+  bounding_boxes.sort(key=lambda x:float(x['confidence']), reverse=True)
+  with open(tmp_files_path + "/" + class_name + "_predictions.json", 'w') as outfile:
+    json.dump(bounding_boxes, outfile)
+
+"""
+ Calculate the AP for each class
+"""
+sum_AP = 0.0
+ap_dictionary = {}
+# open file to store the results
+with open(results_files_path + "/results.txt", 'w') as results_file:
+  results_file.write("# AP and precision/recall per class\n")
+  count_true_positives = {}
+  for class_index, class_name in enumerate(gt_classes):
+    count_true_positives[class_name] = 0
+    """
+     Load predictions of that class
+    """
+    predictions_file = tmp_files_path + "/" + class_name + "_predictions.json"
+    predictions_data = json.load(open(predictions_file))
+
+    """
+     Assign predictions to ground truth objects
+    """
+    nd = len(predictions_data)
+    tp = [0] * nd # creates an array of zeros of size nd
+    fp = [0] * nd
+    for idx, prediction in enumerate(predictions_data):
+      file_id = prediction["file_id"]
+      # assign prediction to ground truth object if any
+      #   open ground-truth with that file_id
+      gt_file = tmp_files_path + "/" + file_id + "_ground_truth.json"
+      ground_truth_data = json.load(open(gt_file))
+      ovmax = -1
+      gt_match = -1
+      # load prediction bounding-box
+      bb = [ float(x) for x in prediction["bbox"].split() ]
+      for obj in ground_truth_data:
+        # look for a class_name match
+        if obj["class_name"] == class_name:
+          bbgt = [ float(x) for x in obj["bbox"].split() ]
+          bi = [max(bb[0],bbgt[0]), max(bb[1],bbgt[1]), min(bb[2],bbgt[2]), min(bb[3],bbgt[3])]
+          iw = bi[2] - bi[0] + 1
+          ih = bi[3] - bi[1] + 1
+          if iw > 0 and ih > 0:
+            # compute overlap (IoU) = area of intersection / area of union
+            ua = (bb[2] - bb[0] + 1) * (bb[3] - bb[1] + 1) + (bbgt[2] - bbgt[0]
+                    + 1) * (bbgt[3] - bbgt[1] + 1) - iw * ih
+            ov = iw * ih / ua
+            if ov > ovmax:
+              ovmax = ov
+              gt_match = obj
+
+      # assign prediction as true positive/don't care/false positive
+
+      # set minimum overlap
+      if ovmax >= min_overlap:
+        if "difficult" not in gt_match:
+            if not bool(gt_match["used"]):
+              # true positive
+              tp[idx] = 1
+              gt_match["used"] = True
+              count_true_positives[class_name] += 1
+              # update the ".json" file
+              with open(gt_file, 'w') as f:
+                  f.write(json.dumps(ground_truth_data))
+            else:
+              # false positive (multiple detection)
+              fp[idx] = 1
+      else:
+        # false positive
+        fp[idx] = 1
+        if ovmax > 0:
+          status = "INSUFFICIENT OVERLAP"
+
+    #print(tp)
+    # compute precision/recall
+    cumsum = 0
+    for idx, val in enumerate(fp):
+      fp[idx] += cumsum
+      cumsum += val
+    cumsum = 0
+    for idx, val in enumerate(tp):
+      tp[idx] += cumsum
+      cumsum += val
+    #print(tp)
+    rec = tp[:]
+    for idx, val in enumerate(tp):
+      rec[idx] = float(tp[idx]) / gt_counter_per_class[class_name]
+    #print(rec)
+    prec = tp[:]
+    for idx, val in enumerate(tp):
+      prec[idx] = float(tp[idx]) / (fp[idx] + tp[idx])
+    #print(prec)
+
+    ap, mrec, mprec = voc_ap(rec, prec)
+    sum_AP += ap
+    text = "{0:.2f}%".format(ap*100) + " = " + class_name + " AP  " #class_name + " AP = {0:.2f}%".format(ap*100)
+    """
+     Write to results.txt
+    """
+    rounded_prec = [ '%.2f' % elem for elem in prec ]
+    rounded_rec = [ '%.2f' % elem for elem in rec ]
+    results_file.write(text + "\n Precision: " + str(rounded_prec) + "\n Recall   :" + str(rounded_rec) + "\n\n")
+    print(text)
+    ap_dictionary[class_name] = ap
+
+    """
+     Draw plot
+    """
+    if save_plots:
+      plt.plot(rec, prec, '-o')
+      # add a new penultimate point to the list (mrec[-2], 0.0)
+      # since the last line segment (and respective area) do not affect the AP value
+      area_under_curve_x = mrec[:-1] + [mrec[-2]] + [mrec[-1]]
+      area_under_curve_y = mprec[:-1] + [0.0] + [mprec[-1]]
+      plt.fill_between(area_under_curve_x, 0, area_under_curve_y, alpha=0.2, edgecolor='r')
+      # set window title
+      fig = plt.gcf() # gcf - get current figure
+      fig.canvas.set_window_title('AP ' + class_name)
+      # set plot title
+      plt.title('class: ' + text)
+      #plt.suptitle('This is a somewhat long figure title', fontsize=16)
+      # set axis titles
+      plt.xlabel('Recall')
+      plt.ylabel('Precision')
+      # optional - set axes
+      axes = plt.gca() # gca - get current axes
+      axes.set_xlim([0.0,1.0])
+      axes.set_ylim([0.0,1.05]) # .05 to give some extra space
+      # Alternative option -> wait for button to be pressed
+      #while not plt.waitforbuttonpress(): pass # wait for key display
+      # Alternative option -> normal display
+      #plt.show()
+      # save the plot
+      fig.savefig(results_files_path + "/classes/" + class_name + ".png")
+      plt.cla() # clear axes for next plot
+
+  results_file.write("\n# mAP of all classes\n")
+  mAP = sum_AP / n_classes
+  text = "mAP = {0:.2f}%".format(mAP*100)
+  results_file.write(text + "\n")
+  print(text)
+
+# remove the tmp_files directory
+shutil.rmtree(tmp_files_path)
+
+"""
+ Count total of Predictions
+"""
+# iterate through all the files
+pred_counter_per_class = {}
+for txt_file in predicted_files_list:
+  # get lines to list
+  lines_list = file_lines_to_list(txt_file)
+  for line in lines_list:
+    class_name = line.split()[0]
+    # check if class is in the classes_to_ignore list, if yes skip
+    if class_name in classes_to_ignore:
+      continue
+    # count that object
+    if class_name in pred_counter_per_class:
+      pred_counter_per_class[class_name] += 1
+    else:
+      # if class didn't exist yet
+      pred_counter_per_class[class_name] = 1
+
+pred_classes = list(pred_counter_per_class.keys())
+
+
+"""
+ Plot the total number of occurences of each class in the ground-truth
+"""
+if save_plots:
+  window_title = "Ground-Truth Info"
+  plot_title = "Ground-Truth\n"
+  plot_title += "(" + str(len(ground_truth_files_list)) + " files and " + str(n_classes) + " classes)"
+  x_label = "Number of objects per class"
+  output_path = results_files_path + "/Ground-Truth Info.png"
+  plot_color = 'forestgreen'
+  draw_plot_func(
+    gt_counter_per_class,
+    n_classes,
+    window_title,
+    plot_title,
+    x_label,
+    output_path,
+    show_plots,
+    plot_color,
+    '',
+    )
+
+"""
+ Write number of ground-truth objects per class to results.txt
+"""
+with open(results_files_path + "/results.txt", 'a') as results_file:
+  results_file.write("\n# Number of ground-truth objects per class\n")
+  for class_name in sorted(gt_counter_per_class):
+    results_file.write(class_name + ": " + str(gt_counter_per_class[class_name]) + "\n")
+
+"""
+ Finish counting true positives
+"""
+for class_name in pred_classes:
+  # if class exists in predictions but not in ground-truth then there are no true positives in that class
+  if class_name not in gt_classes:
+    count_true_positives[class_name] = 0
+
+
+"""
+ Plot the total number of occurences of each class in the "predicted" folder
+"""
+if save_plots:
+  window_title = "Predicted Objects Info"
+  # Plot title
+  plot_title = "Predicted Objects\n"
+  plot_title += "(" + str(len(predicted_files_list)) + " files and "
+  count_non_zero_values_in_dictionary = sum(int(x) > 0 for x in list(pred_counter_per_class.values()))
+  plot_title += str(count_non_zero_values_in_dictionary) + " detected classes)"
+  # end Plot title
+  x_label = "Number of objects per class"
+  output_path = results_files_path + "/Predicted Objects Info.png"
+  plot_color = 'forestgreen'
+  true_p_bar = count_true_positives
+  draw_plot_func(
+    pred_counter_per_class,
+    len(pred_counter_per_class),
+    window_title,
+    plot_title,
+    x_label,
+    output_path,
+    show_plots,
+    plot_color,
+    true_p_bar
+    )
+
+"""
+ Write number of predicted objects per class to results.txt
+"""
+with open(results_files_path + "/results", 'a') as results_file:
+  results_file.write("\n# Number of predicted objects per class\n")
+  for class_name in sorted(pred_classes):
+    n_pred = pred_counter_per_class[class_name]
+    text = class_name + ": " + str(n_pred)
+    text += " (tp:" + str(count_true_positives[class_name]) + ""
+    text += ", fp:" + str(n_pred - count_true_positives[class_name]) + ")\n"
+    results_file.write(text)
+
+"""
+ Draw mAP plot (Show AP's of all classes in decreasing order)
+"""
+if save_plots:
+  window_title = "mAP"
+  plot_title = "mAP = {0:.2f}%".format(mAP*100)
+  x_label = "Average Precision"
+  output_path = results_files_path + "/mAP.png"
+  plot_color = 'royalblue'
+  draw_plot_func(
+    ap_dictionary,
+    n_classes,
+    window_title,
+    plot_title,
+    x_label,
+    output_path,
+    show_plots,
+    plot_color,
+    ""
+    )
